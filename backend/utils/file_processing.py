086d854e9436770ee3473c52b5fde478f47887ef
Valerie Song <valsong88@gmail.com>
2025-09-27 02:31:46 -0400

    changed to python magic

--- backend/utils/file_processing.py
+++ backend/utils/file_processing.py
@@ -4,19 +4,19 @@ import magic
 import requests
 import re
 
-ALLOWED_EXTENSIONS = {'.pdf', '.doc', '.txt'}
+def load_document(url):
+    mime = magic.Magic(mime=True)
+    file_type = mime.from_file(url)
+    if not any(ext in file_type for ext in ["pdf", "word", "text"]):
+        raise ValueError(f"Unsupported file type: {file_type}")
+    print(file_type)
 
-# def load_document(url):
-#     mime = magic.Magic(mime=True)
-#     file_type = mime.from_file(url)
-#     print(file_type)
-
-def load_document(file_path) -> str:
-    ext = os.path.splitext(file_path)[1].lower()
-    if ext not in ALLOWED_EXTENSIONS:
-        raise ValueError(f"Unsupported file type: {ext}")
-    else: 
-        return convert_file_to_md(file_path)
+# def load_document(file_path) -> str:
+#     ext = os.path.splitext(file_path)[1].lower()
+#     if ext not in ALLOWED_EXTENSIONS:
+#         raise ValueError(f"Unsupported file type: {ext}")
+#     else: 
+#         return convert_file_to_md(file_path)
 
 def clean_text(raw_text: str) -> str:
     text = re.sub(r'\s+', ' ', raw_text)
@@ -31,7 +31,7 @@ def convert_file_to_md(source_file: DocumentStream) -> str:
     cleantext = doc.export_to_markdown()
     return clean_text(cleantext)
 
-print(load_document('/Users/valeriesong/Downloads/Remarkably Bright Creatures Week I (Sept 24-26) Response Sheet.pdf'))
-#load_document('https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png')
+#load_document('/Users/valeriesong/Downloads/Remarkably Bright Creatures Week I (Sept 24-26) Response Sheet.pdf')
+load_document("/Users/valeriesong/Downloads/mongodb-compass-1.46.11-darwin-arm64.dmg")
 
 # image processing
